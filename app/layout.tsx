7a53a94161027523f69dc3506de54d81ef03ef17
Vedang Purohit <z5592103@ad.unsw.edu.au>
2025-12-13 13:54:40 +1100

    Editor stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+          src="https://cdn.jsdelivr.net/pyodide/v0.24.1/full/pyodide.js"
+          strategy="afterInteractive"
+        />
         {children}
       </body>
     </html>
